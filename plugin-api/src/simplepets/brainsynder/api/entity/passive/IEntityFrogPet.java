6ac374c366459b5aff1b789dc46625c9e0ac290d
brainsynder <briansnyder498@gmail.com>
2024-04-26 21:26:30 -0400

    refactor: Renamed FrogVariant to FrogType

--- plugin-api/src/simplepets/brainsynder/api/entity/passive/IEntityFrogPet.java
+++ plugin-api/src/simplepets/brainsynder/api/entity/passive/IEntityFrogPet.java
@@ -5,13 +5,13 @@ import lib.brainsynder.SupportedVersion;
 import simplepets.brainsynder.api.entity.misc.EntityPetType;
 import simplepets.brainsynder.api.entity.misc.IAgeablePet;
 import simplepets.brainsynder.api.pet.PetType;
-import simplepets.brainsynder.api.wrappers.FrogVariant;
+import simplepets.brainsynder.api.wrappers.FrogType;
 
 @SupportedVersion(version = ServerVersion.v1_19)
 @EntityPetType(petType = PetType.FROG)
 public interface IEntityFrogPet extends IAgeablePet {
-    void setVariant (FrogVariant variant);
-    FrogVariant getVariant ();
+    void setVariant (FrogType variant);
+    FrogType getVariant ();
 
     boolean isCroaking ();
     void setCroaking (boolean value);
--- plugin-api/src/simplepets/brainsynder/api/pet/data/frog/FrogVariantData.java
+++ plugin-api/src/simplepets/brainsynder/api/pet/data/frog/FrogVariantData.java
@@ -5,12 +5,12 @@ import org.bukkit.Material;
 import simplepets.brainsynder.api.Namespace;
 import simplepets.brainsynder.api.entity.passive.IEntityFrogPet;
 import simplepets.brainsynder.api.pet.PetData;
-import simplepets.brainsynder.api.wrappers.FrogVariant;
+import simplepets.brainsynder.api.wrappers.FrogType;
 
 @Namespace(namespace = "variant")
 public class FrogVariantData extends PetData<IEntityFrogPet> {
     public FrogVariantData() {
-        for (FrogVariant type : FrogVariant.values()) {
+        for (FrogType type : FrogType.values()) {
             addDefaultItem(type.name(), new ItemBuilder(Material.PLAYER_HEAD)
                     .withName("&#c8c8c8{name}: &a"+type.name())
                     .setTexture(type.getTexture()));
@@ -19,12 +19,12 @@ public class FrogVariantData extends PetData<IEntityFrogPet> {
 
     @Override
     public Object getDefaultValue() {
-        return FrogVariant.TEMPERATE;
+        return FrogType.TEMPERATE;
     }
 
     @Override
     public void onLeftClick(IEntityFrogPet entity) {
-        entity.setVariant(FrogVariant.getNext(entity.getVariant()));
+        entity.setVariant(FrogType.getNext(entity.getVariant()));
     }
 
     @Override
