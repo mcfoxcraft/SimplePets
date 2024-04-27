9f4e3ab41ef1df0bf678a0dabd76cd7688c8f06e
brainsynder <briansnyder498@gmail.com>
2024-04-26 21:26:56 -0400

    refactor: Renamed VillagerData to VillagerInfo

--- plugin-api/src/simplepets/brainsynder/api/entity/misc/IProfession.java
+++ plugin-api/src/simplepets/brainsynder/api/entity/misc/IProfession.java
@@ -2,7 +2,7 @@ package simplepets.brainsynder.api.entity.misc;
 
 
 import simplepets.brainsynder.api.wrappers.villager.BiomeType;
-import simplepets.brainsynder.api.wrappers.villager.VillagerData;
+import simplepets.brainsynder.api.wrappers.villager.VillagerInfo;
 import simplepets.brainsynder.api.wrappers.villager.VillagerLevel;
 import simplepets.brainsynder.api.wrappers.villager.VillagerType;
 
@@ -10,8 +10,8 @@ public interface IProfession {
     /**
      * This gets used for 1.14+
      */
-    default VillagerData getVillagerData () {
-        return VillagerData.getDefault();
+    default VillagerInfo getVillagerData () {
+        return VillagerInfo.getDefault();
     }
     default VillagerType getVillagerType() {
         return getVillagerData().getType();
@@ -26,7 +26,7 @@ public interface IProfession {
     /**
      * This gets used for 1.14+
      */
-    default void setVillagerData (VillagerData data){}
+    default void setVillagerData (VillagerInfo data){}
     default void setVillagerType (VillagerType type){
         setVillagerData(getVillagerData().withType(type));
     }
--- plugin-api/src/simplepets/brainsynder/api/wrappers/villager/VillagerInfo.java
+++ plugin-api/src/simplepets/brainsynder/api/wrappers/villager/VillagerInfo.java
@@ -5,12 +5,12 @@ import lib.brainsynder.nbt.StorageTagCompound;
 /**
  * Will store the Villager information (1.14+)
  */
-public class VillagerData {
+public class VillagerInfo {
     private BiomeType biome = BiomeType.PLAINS;
     private VillagerType type = VillagerType.NONE;
     private VillagerLevel level = VillagerLevel.NOVICE;
 
-    public VillagerData(BiomeType biome, VillagerType type, VillagerLevel level) {
+    public VillagerInfo(BiomeType biome, VillagerType type, VillagerLevel level) {
         this.biome = biome;
         this.type = type;
         this.level = level;
@@ -28,16 +28,16 @@ public class VillagerData {
         return this.level;
     }
 
-    public VillagerData withBiome(BiomeType biome) {
-        return new VillagerData(biome, this.type, this.level);
+    public VillagerInfo withBiome(BiomeType biome) {
+        return new VillagerInfo(biome, this.type, this.level);
     }
 
-    public VillagerData withType(VillagerType type) {
-        return new VillagerData(this.biome, type, this.level);
+    public VillagerInfo withType(VillagerType type) {
+        return new VillagerInfo(this.biome, type, this.level);
     }
 
-    public VillagerData withLevel(VillagerLevel level) {
-        return new VillagerData(this.biome, this.type, level);
+    public VillagerInfo withLevel(VillagerLevel level) {
+        return new VillagerInfo(this.biome, this.type, level);
     }
 
     public StorageTagCompound toCompound () {
@@ -48,11 +48,11 @@ public class VillagerData {
         return compound;
     }
 
-    public static VillagerData getDefault () {
-        return new VillagerData(BiomeType.PLAINS, VillagerType.NONE, VillagerLevel.NOVICE);
+    public static VillagerInfo getDefault () {
+        return new VillagerInfo(BiomeType.PLAINS, VillagerType.NONE, VillagerLevel.NOVICE);
     }
 
-    public static VillagerData fromCompound (StorageTagCompound compound) {
+    public static VillagerInfo fromCompound (StorageTagCompound compound) {
         BiomeType biome = BiomeType.PLAINS;
         if (compound.hasKey("biome")) biome = compound.getEnum("biome", BiomeType.class, BiomeType.PLAINS);
 
@@ -62,6 +62,6 @@ public class VillagerData {
         VillagerLevel level = VillagerLevel.NOVICE;
         if (compound.hasKey("level")) level = compound.getEnum("level", VillagerLevel.class, VillagerLevel.NOVICE);
 
-        return new VillagerData(biome, type, level);
+        return new VillagerInfo(biome, type, level);
     }
 }
