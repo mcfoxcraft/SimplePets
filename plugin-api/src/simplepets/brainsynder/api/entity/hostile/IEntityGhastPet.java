7d0cd70e7894585e264d426656c8d82d100927ab
brainsynder <briansnyder498@gmail.com>
2024-05-07 23:33:00 -0400

    refactor: so the ghast and bat pets were never labeled as 'Flyable'

--- plugin-api/src/simplepets/brainsynder/api/entity/hostile/IEntityGhastPet.java
+++ plugin-api/src/simplepets/brainsynder/api/entity/hostile/IEntityGhastPet.java
@@ -2,10 +2,11 @@ package simplepets.brainsynder.api.entity.hostile;
 
 import simplepets.brainsynder.api.entity.IEntityPet;
 import simplepets.brainsynder.api.entity.misc.EntityPetType;
+import simplepets.brainsynder.api.entity.misc.IFlyableEntity;
 import simplepets.brainsynder.api.pet.PetType;
 
 @EntityPetType(petType = PetType.GHAST)
-public interface IEntityGhastPet extends IEntityPet {
+public interface IEntityGhastPet extends IEntityPet, IFlyableEntity {
     @Override
     default boolean isBig() {
         return true;
--- plugin-api/src/simplepets/brainsynder/api/entity/passive/IEntityBatPet.java
+++ plugin-api/src/simplepets/brainsynder/api/entity/passive/IEntityBatPet.java
@@ -2,10 +2,11 @@ package simplepets.brainsynder.api.entity.passive;
 
 import simplepets.brainsynder.api.entity.IEntityPet;
 import simplepets.brainsynder.api.entity.misc.EntityPetType;
+import simplepets.brainsynder.api.entity.misc.IFlyableEntity;
 import simplepets.brainsynder.api.pet.PetType;
 
 @EntityPetType(petType = PetType.BAT)
-public interface IEntityBatPet extends IEntityPet {
+public interface IEntityBatPet extends IEntityPet, IFlyableEntity {
     boolean isHanging();
 
     void setHanging(boolean var1);
